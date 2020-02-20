954b6b3c4812b399a7997044c37d6dbbfa0e29ba
Justin Dennis <jxd8037@g.rit.edu>
2020-02-19 22:31:53 -0500

    Updated render pipeline

---
+++ ShaderGraphPresentation/Assets/Scripts/Dissolve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    private Material material;
+    [SerializeField]
+    private float dissolveTime;
+
+    public float DissolveAmount 
+    {
+        get { return material.GetFloat("_DissolveAmount"); }
+        set { material.SetFloat("_DissovleAmount", value); }
+    }
+
+    void Awake()
+    {
+        material = GetComponent<MeshRenderer>().materials[0];
+    }
+
+    private void Update()
+    {
+        
+    }
+}
